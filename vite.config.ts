73747d0617c5f47c75e9896c7170c50a9e7869c9
nhatnamduong688 <nhatnamduong688@gmail.com>
2025-03-20 23:45:56 +0700

    chore: update devDependencies and refactor Vite configuration
    - Added `@types/node` to devDependencies for improved TypeScript support.
    - Refactored Vite configuration to use CommonJS syntax for path resolution and improved aliasing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
+const path = require('path')
+
+const __dirname = new URL('.', import.meta.url).pathname
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
 })
